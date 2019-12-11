0bcdb3094a74835774c4d80ba2a5d4bfb73325ce
emnono <45090671+emnono@users.noreply.github.com>
2019-12-11 06:42:32 +0100

    display movies on screen device

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -4,9 +4,8 @@ export function getFilmsFromApiWithSearchedText(text) {
   const url =
     "https://api.themoviedb.org/3/search/movie?api_key=" +
     API_TOKEN +
-    "&language=en-US&query=" +
+    "&language=fr&query=" +
     text;
-  //https://api.themoviedb.org/3/search/movie?api_key=c9e0bce44b256e722e7fc17bd12c2594&language=en-US&query=star
   //fetch is a JS librairy that return 2 options
   return fetch(url)
     .then(response => response.json())
--- Components/Search.js
+++ Components/Search.js
@@ -12,16 +12,16 @@ import FilmItem from "./FilmItem";
 import { getFilmsFromApiWithSearchedText } from "../API/TMDBApi";
 
 class Search extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { films: [] };
-  // }
+  constructor(props) {
+    super(props);
+    this.state = { films: [] };
+  }
 
   _loadFilms() {
     getFilmsFromApiWithSearchedText("star").then(data => console.log(data));
-    // getFilmsFromApiWithSearchedText("star").then(data =>
-    //   this.setState({ films: data.results })
-    // );
+    getFilmsFromApiWithSearchedText("star").then(data =>
+      this.setState({ films: data.results })
+    );
   }
 
   render() {
@@ -34,7 +34,7 @@ class Search extends React.Component {
           onPress={() => this._loadFilms()}
         />
         <FlatList
-          data={films}
+          data={this.state.films}
           keyExtractor={item => item.id.toString()}
           renderItem={({ item }) => <FilmItem film={item} />}
         />
